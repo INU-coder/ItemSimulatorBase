fd7ad43608b38306325eb25890c261ebf355302f
INU-coder <tothel56@gmail.com>
2024-09-09 21:01:30 +0900

    console.error
    // 2. 데이터베이스에 아이템 저장하기 error.

--- routes/item.js
+++ routes/item.js
@@ -12,22 +12,35 @@ const prisma = new PrismaClient({
 // [필수] 1. 아이템 생성
 // 1. 아이템 코드, 아이템 명, 아이템 능력, 아이템 가격을 req(request)에서 json으로 전달받기
 // 2. 데이터베이스에 아이템 저장하기
-router.post('/item/create', async (req, res) => {
-  const itemCode = req.body.item_code;
-  const itemName = req.body.item_name;
-  const atk = req.body.atk;
-  const price = req.body.price;
+router.post('/item/create/:itemId', async (req, res) => {
+  try {
+    const itemCode = req.body.item_code;
+    const itemName = req.body.item_name;
+    const atk = req.body.atk;
+    const price = req.body.price;
 
-  const createItem = prisma.item.create({
-    data: {
-      itemCode: itemCode,
-      itemName: itemName,
-      atk: atk,
-      price: price,
-    },
+    const createItem = await prisma.item.create({
+      data: {
+        itemCode: itemCode,
+        itemName: itemName,
+        atk: atk,
+        price: price,
+      }, 
+    });
+
+    res.status(200).json({ item_info: createItem });
+    console.log(createItem);
+   } catch (error) { // 클라이언트에게 에러 코드 자체를 출력
+   console.error("에러가 발생 했습니다.", error);
+
+   res.status(500).json({
+    error: '실패했습니다.',
+    message: error.message || '알 수 없는 오류가 발생했습니다.'
+    // catch (error) { 
+    res.status(500).json({ error: error})
+   console.log(error);
   });
-
-  console.log(createItem);
+}
 });
   // console.log('item create');
 
